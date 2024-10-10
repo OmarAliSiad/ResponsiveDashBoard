e9fa09322fdefa340ff8814529e749bae6c0ec46
omarali <omaraliabdelaziz250@gmail.com>
2024-10-10 14:17:20 +0300

    custom settings like logut and settings

---
+++ lib/features/presenation/widgets/custom_settings.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/active_and_incative_item.dart';
+
+import '../../../core/utils/assets.dart';
+import '../../data/models/drawer_item.dart';
+
+class CustomSettings extends StatelessWidget {
+  const CustomSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InActiveDrawerItem(
+          drawerItemModel: DrawerItemModel(
+              title: 'Setting system', image: Assets.imagesSettings),
+        ),
+        InActiveDrawerItem(
+          drawerItemModel: DrawerItemModel(
+              title: 'Logout account', image: Assets.imagesLogout),
+        ),
+      ],
+    );
+  }
+}
