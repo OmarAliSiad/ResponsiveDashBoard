f81fd800bf691f22edded7cdc9ba6d0ea546508e
omarali <omaraliabdelaziz250@gmail.com>
2024-10-10 14:11:07 +0300

    active and inactive drawer item

---
+++ lib/features/presenation/widgets/active_and_incative_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../../core/utils/app_styles.dart';
+import '../../data/models/drawer_item.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: drawerItemModel.textStyle ?? AppStyles.styleRegular16(),
+      ),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleBold16(),
+      ),
+      trailing: Container(
+        width: 3.27,
+        decoration: const BoxDecoration(color: Color(0xff4EB7F2)),
+      ),
+    );
+  }
+}
