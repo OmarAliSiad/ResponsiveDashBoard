c4aa36d7b53d9971acc499087e182ff12467ca4e
omarali <omaraliabdelaziz250@gmail.com>
2024-10-10 14:11:27 +0300

    drawer list view to display all drawer items

---
+++ lib/features/data/models/drawer_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerItemModel {
+  final String title;
+  final String image;
+  final TextStyle ? textStyle;
+
+  DrawerItemModel(
+      {required this.title, required this.image,  this.textStyle});
+}
