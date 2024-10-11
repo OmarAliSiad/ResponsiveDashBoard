d6a9a6bc331154d6a58fd676825dc0f55a1e2147
omarali <omaraliabdelaziz250@gmail.com>
2024-10-11 23:29:10 +0300

    all expenses that display header and all expenses item list view

--- lib/features/presenation/widgets/all_expenses.dart
+++ lib/features/presenation/widgets/all_expenses.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/all_expenses_header.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/all_expenses_item_list_view.dart';
 
+import 'custom_container_back_ ground.dart';
+
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-          color:  Colors.white,
-          borderRadius: BorderRadius.circular(12)),
-      child: const Column(
+    return const CustomBackGroundContainer(
+      padding: 20,
+      child: Column(
         children: [
           SizedBox(
             height: 20,
