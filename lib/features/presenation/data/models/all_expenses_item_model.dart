ff4c0452db72b5e22e66f4c8c7ef937460d2e73e
omarali <omaraliabdelaziz250@gmail.com>
2024-10-11 16:37:04 +0300

    model for expeneses item

---
+++ lib/features/presenation/data/models/all_expenses_item_model.dart
@@ -0,0 +1,10 @@
+class AllExpensesItemModel {
+  final String title;
+  final String image;
+  final String date;
+  final String price;
+
+  const AllExpensesItemModel({required this.title, required this.image, required this.date, required this.price});
+
+
+}
