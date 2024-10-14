caca06655e6737f3f843652c413c23b74f13dd88
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 23:55:21 +0300

    make custom container back ground widget with padding and margin properties

--- lib/features/presenation/widgets/custom_container_back_ ground.dart
+++ lib/features/presenation/widgets/custom_container_back_ ground.dart
@@ -2,19 +2,36 @@ import 'package:flutter/material.dart';
 
 class CustomBackGroundContainer extends StatelessWidget {
   final Widget child;
-  final double padding;
+  final double paddingHorizontal;
+  final double paddingVertical;
+  final double marginLeft;
+  final double marginRight;
+  final double marginTop, marginBottom;
   const CustomBackGroundContainer({
     super.key,
     required this.child,
-    required this.padding,
+    required this.paddingHorizontal,
+    required this.paddingVertical,
+    required this.marginTop,
+    required this.marginBottom,
+    required this.marginLeft,
+    required this.marginRight,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: padding ?? 20),
+      margin: EdgeInsets.only(
+          top: marginTop,
+          bottom: marginBottom,
+          left: marginLeft,
+          right: marginRight),
+      padding: EdgeInsets.symmetric(
+          horizontal: paddingHorizontal ?? 20, vertical: paddingVertical ?? 0),
       decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(12)),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: child,
     );
   }
