b21376ae4f8880240eb4c999899b4901f59471e1
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 23:53:07 +0300

    user info tile

--- lib/features/presenation/widgets/user_info_tile.dart
+++ lib/features/presenation/widgets/user_info_tile.dart
@@ -4,12 +4,8 @@ import 'package:responsive_dash_board/core/utils/app_styles.dart';
 import 'package:responsive_dash_board/features/data/models/user_info_model.dart';
 
 class UserInfoTile extends StatelessWidget {
-
-
-  final UserInfoModel userInfoModel; 
-  const UserInfoTile(
-      {super.key,
-     required this.userInfoModel});
+  final UserInfoModel userInfoModel;
+  const UserInfoTile({super.key, required this.userInfoModel});
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +13,23 @@ class UserInfoTile extends StatelessWidget {
       color: const Color(0xffFAFAFA),
       elevation: 0,
       child: ListTile(
-          leading: SvgPicture.asset(userInfoModel.image),
-          title: Text(userInfoModel.title, style: AppStyles.styleSemiBold16()),
-          subtitle: Text(userInfoModel.subTitle, style: AppStyles.styleRegular14())),
+        leading: SvgPicture.asset(userInfoModel.image!),
+        title: SizedBox(
+          width: 123,
+          height: 20,
+          child: Text(userInfoModel.title, style: AppStyles.styleSemiBold16()),
+        ),
+        subtitle: FittedBox(
+          alignment: Alignment.centerLeft,
+          fit: BoxFit.scaleDown,
+          child: SizedBox(
+            width: 139,
+            height: 15,
+            child:
+                Text(userInfoModel.subTitle, style: AppStyles.styleRegular14()),
+          ),
+        ),
+      ),
     );
   }
 }
