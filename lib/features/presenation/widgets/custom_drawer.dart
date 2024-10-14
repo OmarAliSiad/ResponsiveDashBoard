6ec3b75bc9cb756c7c6f72a76a3bc5cd583f0391
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 22:17:04 +0300

    custom drawer widget that takes user info model  and wrap all of this in custom scroll view

--- lib/features/presenation/widgets/custom_drawer.dart
+++ lib/features/presenation/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/data/models/user_info_model.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/custom_settings.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/user_info_tile.dart';
@@ -11,23 +12,25 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const CustomScrollView(
-        physics: BouncingScrollPhysics(),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
           SliverToBoxAdapter(
             child: UserInfoTile(
-              title: 'Lekan Okeowo',
-              subTitle: 'demo@gmail.com',
-              image: Assets.imagesAvatar3,
+              userInfoModel: UserInfoModel(
+                  image: Assets.imagesAvatar3,
+                  title: 'Lekan Okeowo',
+                  subTitle: 'demo@gmail.com'),
             ),
           ),
-          SliverToBoxAdapter(
+          const SliverToBoxAdapter(
             child: SizedBox(
               height: 20,
             ),
           ),
-          DrawerItmesListView(),
-          SliverFillRemaining(hasScrollBody: false, child: CustomSettings()),
+          const DrawerItmesListView(),
+          const SliverFillRemaining(
+              hasScrollBody: false, child: CustomSettings()),
         ],
       ),
     );
