570139f6d264f582841b93aa7e77fac7cc5eed0e
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 23:52:30 +0300

    custom text field

---
+++ lib/features/presenation/widgets/custom_text_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../../core/utils/app_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  const CustomTextField({super.key, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: Colors.grey,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.only(left: 20),
+        fillColor: const Color((0xFFFAFAFA)),
+        filled: true,
+        border: BuildOutlineInputBoder(),
+        focusedBorder: BuildOutlineInputBoder(),
+        enabledBorder: BuildOutlineInputBoder(),
+        hintText: hintText,
+        hintStyle: AppStyles.styleRegular16().copyWith(
+          color: const Color(0xffAAAAAA),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder BuildOutlineInputBoder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none);
+  }
+}
