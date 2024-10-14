6ca9223cb2657fd44dcc4a4ec855aad7d987349f
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 23:52:45 +0300

    custom form

---
+++ lib/features/presenation/widgets/custom_form.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/user_info_form.dart';
+
+class CustomForm extends StatelessWidget {
+  const CustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const UserInfo();
+  }
+}
