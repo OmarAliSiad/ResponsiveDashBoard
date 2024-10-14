47420f7e65443e31f11efdf8fb7c3fbcc08e4da6
omarali <omaraliabdelaziz250@gmail.com>
2024-10-14 23:53:54 +0300

    user info model

--- lib/features/data/models/user_info_model.dart
+++ lib/features/data/models/user_info_model.dart
@@ -1,8 +1,8 @@
 class UserInfoModel {
-  final String image;
+  final String ? image;
   final String title;
   final String subTitle;
 
   UserInfoModel(
-      {required this.image, required this.title, required this.subTitle});
+      { this.image, required this.title, required this.subTitle});
 }
