70cfe3771451d8fbe625633c82bd2f73e092d5a3
omarali <omaraliabdelaziz250@gmail.com>
2024-10-17 16:48:24 +0300

    AllExpensesAndQuickInvoiceSection

---
+++ lib/features/presenation/widgets/all_expenses_and_quick_invoice.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/quick_invoice.dart';
+
+class AllExpensesAndQuickInvoiceSection extends StatelessWidget {
+  const AllExpensesAndQuickInvoiceSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      physics: BouncingScrollPhysics(),
+      slivers: [
+        SliverToBoxAdapter(
+          child: AllExpenses(),
+        ),
+        SliverToBoxAdapter(
+          child: QuickInvoice(),
+        )
+      ],
+    );
+  }
+}
