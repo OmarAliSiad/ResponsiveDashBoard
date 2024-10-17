4a8ee3c36d194284a6ab7e9ec80f1765b53481f0
omarali <omaraliabdelaziz250@gmail.com>
2024-10-17 17:00:31 +0300

    add card section

--- lib/features/presenation/widgets/desktop_layout..dart
+++ lib/features/presenation/widgets/desktop_layout..dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/features/presenation/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/all_expenses_and_quick_invoice.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/features/presenation/widgets/quick_invoice.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/my_card.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
@@ -13,19 +13,13 @@ class DesktopLayout extends StatelessWidget {
         Expanded(flex: 1, child: CustomDrawer()),
         SizedBox(width: 32),
         Expanded(
-          flex: 4,
-          child: CustomScrollView(
-            physics: BouncingScrollPhysics(),
-            slivers: [
-              SliverToBoxAdapter(
-                child: AllExpenses(),
-              ),
-              SliverToBoxAdapter(
-                child: QuickInvoice(),
-              )
-            ],
-          ),
+          flex: 3,
+          child: AllExpensesAndQuickInvoiceSection(),
         ),
+        SizedBox(
+          width: 24,
+        ),
+        Expanded(flex: 2, child: MyCard())
       ],
     );
   }
