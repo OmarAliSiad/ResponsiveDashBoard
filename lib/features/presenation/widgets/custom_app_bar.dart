1909b650ae1e085b52e1f2a6c48a50e09d04e3fe
omarali <omaraliabdelaziz250@gmail.com>
2024-10-21 22:19:49 +0300

    Add CustomAppBar widget for transparent app bar with menu icon
    This commit introduces a new `CustomAppBar` widget in the `lib/features/presentation/widgets` directory. The widget is a stateless widget that implements `PreferredSizeWidget` to ensure it fits the standard app bar height. The `CustomAppBar` features a transparent background with no elevation and includes a leading icon button that opens the drawer when pressed. The widget is designed to be flexible and can be easily integrated into any screen that requires a custom app bar with a menu icon.

---
+++ lib/features/presenation/widgets/custom_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final GlobalKey<ScaffoldState> globalKey;
+  const CustomAppBar({super.key, required this.globalKey});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+        icon: const Icon(
+          Icons.menu_outlined,
+          color: Colors.black,
+        ),
+        onPressed: () {
+          globalKey.currentState!.openDrawer();
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
