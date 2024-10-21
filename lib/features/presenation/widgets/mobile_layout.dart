6ab36cfa4de6a9bda1b6b5197276d78d5c2eb0d3
omarali <omaraliabdelaziz250@gmail.com>
2024-10-21 22:20:11 +0300

    Add MobileLayout widget for responsive dashboard
    This commit introduces the `MobileLayout` widget, designed to handle the layout for mobile devices in the responsive dashboard. The widget includes a `CustomAppBar`, a `CustomDrawer`, and a `DashBoardMobileLayout` as its main components. The `Scaffold` is configured with a background color and a global key for managing the state of the app bar and drawer. The drawer is sized to fit mobile screens, ensuring a seamless user experience on smaller devices.

---
+++ lib/features/presenation/widgets/mobile_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/custom_app_bar.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/dash_board_mobile_layout.dart';
+import 'custom_drawer.dart';
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
+    return Scaffold(
+      backgroundColor: const Color(0xffF7F9FA),
+      appBar: CustomAppBar(globalKey: globalKey),
+      key: globalKey,
+      drawer: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: 300,
+        child: const CustomDrawer(),
+      ),
+      body: const DashBoardMobileLayout(),
+    );
+  }
+}
