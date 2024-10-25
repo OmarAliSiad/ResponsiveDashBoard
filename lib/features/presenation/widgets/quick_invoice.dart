cb470b02ac5246aeb1f5e62576e77fac1b8ec3f6
omarali <omaraliabdelaziz250@gmail.com>
2024-10-25 23:26:22 +0300

    quick invoice section

--- lib/features/presenation/widgets/quick_invoice.dart
+++ lib/features/presenation/widgets/quick_invoice.dart
@@ -6,18 +6,31 @@ import 'package:responsive_dash_board/features/presenation/widgets/quick_invoice
 import 'latest_transction.dart';
 
 class QuickInvoice extends StatelessWidget {
-  const QuickInvoice({super.key});
+  final double? marginTop;
+  final double? marginBottom;
+  final double? marginLeft;
+  final double? marginRight;
+  final double? paddingHorizontal;
+  final double? paddingVertical;
+  const QuickInvoice(
+      {super.key,
+      this.marginTop,
+      this.marginBottom,
+      this.marginLeft,
+      this.marginRight,
+      this.paddingHorizontal,
+      this.paddingVertical});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomBackGroundContainer(
-      marginTop: 24,
-      marginBottom: 32,
-      marginRight: 24,
-      marginLeft: 32,
-      paddingHorizontal: 24,
-      paddingVertical: 24,
-      child: Column(
+    return CustomBackGroundContainer(
+      marginTop: marginTop ?? 24,
+      marginBottom: marginBottom ?? 32,
+      marginRight: marginRight ?? 24,
+      marginLeft: marginLeft ?? 32,
+      paddingHorizontal: paddingHorizontal ?? 24,
+      paddingVertical: paddingVertical ?? 24,
+      child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           QuickInvoiceHeader(),
