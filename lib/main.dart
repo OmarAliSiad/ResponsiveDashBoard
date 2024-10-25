af2be986608851fccaaf98c14a040369aaebeb6c
omarali <omaraliabdelaziz250@gmail.com>
2024-10-25 23:21:23 +0300

    This commit introduces entry point for applications that contains material app scroll behavior and DashBoardView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'features/presenation/views/dash_board_view.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) => const ResponsiveDashBoard()));
+  runApp(const ResponsiveDashBoard());
 }
 
 class ResponsiveDashBoard extends StatelessWidget {
@@ -14,9 +12,6 @@ class ResponsiveDashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: false,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       scrollBehavior: const MaterialScrollBehavior().copyWith(
         dragDevices: {
