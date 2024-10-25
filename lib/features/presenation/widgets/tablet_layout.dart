0df168dd35ceda93f4319198e4baf1bae13250c2
omarali <omaraliabdelaziz250@gmail.com>
2024-10-25 23:56:54 +0300

    tablet layout that contains two Expanded widget inside row widget customDrawer and DashBoardTabeltLayout

--- lib/features/presenation/widgets/tablet_layout.dart
+++ lib/features/presenation/widgets/tablet_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/features/presenation/widgets/dash_board_layout.dart';
 import 'package:responsive_dash_board/features/presenation/widgets/dash_board_mobile_layout.dart';
 
 class TabletLayout extends StatelessWidget {
@@ -10,7 +11,7 @@ class TabletLayout extends StatelessWidget {
     return const Row(
       children: [
         Expanded(flex: 1, child: CustomDrawer()),
-        Expanded(flex: 3, child: DashBoardMobileLayout()),
+        Expanded(flex: 3, child: DashBoardTabeltLayout()),
       ],
     );
   }
